ec0dcefcf333cb577dc82062b120a5962f978a85
Balaji <balajiadake00@gmail.com>
2026-05-13 11:06:05 +0530

    initial commit

---
+++ basicdatatype.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+
+    scanf("%d %lld %c %f %lf",&a, &b,&c, &d, &e);
+    printf("%d\n", a);
+    printf("%lld\n", b);
+    printf("%c\n", c);
+    printf("%f\n", d);
+    printf("%lf\n", e);
+
+    return 0;
+}
+
+   
+
+
