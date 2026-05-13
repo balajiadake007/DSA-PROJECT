e0d3c408761e26070e727e1b6ccdf54198ed5734
Balaji <balajiadake00@gmail.com>
2026-05-13 12:40:07 +0530

    conditional_statement

--- basicdatatype.cpp
+++ basicdatatype.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <iostream>
 
 using namespace std;
@@ -23,3 +24,30 @@ int main(){
    
 
 
+=======
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+
+    scanf("%d %lld %c %f %lf",&a, &b,&c, &d, &e);
+    printf("%d\n", a);
+    printf("%lld\n", b);
+    printf("%c\n", c);
+    printf("%f\n", d);
+    printf("%lf\n", e);
+
+    return 0;
+}
+
+   
+
+
+>>>>>>> 18c0672fd2d1c1a97aa9130d033b841fceee4f87
